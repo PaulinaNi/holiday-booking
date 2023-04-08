30751178cbac120ab5d18d59b2e44d7a7a20c7f4
Paulina <paulina.justyna.niedzielska@gmail.com>
2023-04-08 20:44:03 +0100

    holiday reqests history page

--- src/components/requestsHistory/requestsHistory.component.jsx
+++ src/components/requestsHistory/requestsHistory.component.jsx
@@ -8,7 +8,9 @@ import { collection, getDocs } from "firebase/firestore"
 
 
 export default function RequestsHisatory() {
+ //employee data passed from previous component
  const { state } = useLocation()
+
  const [allHolidayRequests, setAllHolidayRequests] = useState()
 
  useEffect(() => {
@@ -28,7 +30,22 @@ export default function RequestsHisatory() {
  return (
   <section>
    <Link className="buttonContainer link" to="/" state={{ ...state }}>Homepage</Link>
-   <p>Hi there</p>
+   {allHolidayRequests && allHolidayRequests.filter(holiday => holiday.employeeId === state.id).map(holiday => {
+    return (
+     <section className="requestCardContainer reqestsHistoryContainer">
+      <h1 className="requestCardContainer-header">{state.firstname} {state.lastname}'s Holiday Request</h1>
+      {holiday.isAccepted && <span className='acceptedLabel'>Accepted</span>}
+      {holiday.isRejected && <span className='rejectedLabel'>Rejected</span>}
+      <p>
+       Holiday from <span className="requestCardContainer-dates">{holiday.startDate.toDate().toDateString()}</span> to <span className="requestCardContainer-dates">{holiday.endDate.toDate().toDateString()}</span>
+      </p>
+      <div className="requestCardContainer-commentContainer">
+       <p className="requestCardContainer-commentHeader">Request Comment:</p>
+       <p className="requestCardContainer-comment">{holiday.comment}</p>
+      </div>
+     </section>
+    )
+   })}
   </section>
  )
 }
